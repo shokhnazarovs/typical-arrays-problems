8e5b1693b5e737129e94e27e146eb641414bc34d
Shokhrukh Shokhnazarov <s.shokhnazarov@gmail.com>
2020-03-17 22:44:05 +0500

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    if(array.length === 0)
+        return 0;
+
+    let smallest = array[0];
+
+    for(let i=0; i<array.length; i++){
+        if(array[i] < smallest)
+            smallest = array[i];
+    }
+    return smallest;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    if(array.length === 0)
+        return 0;
+
+    let biggest = array[0];
+
+    for(let i=0; i<array.length; i++){
+        if(array[i] > biggest)
+            biggest = array[i];
+    }
+    return biggest;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+    if(array.length === 0)
+        return 0;
+
+    let sum = 0;
+
+    for(let i=0; i<array.length; i++){
+        sum = sum + array[i];
+    }
+
+    return sum/array.length;
 }
